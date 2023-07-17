4aee81d3bba6a61dcc3556f61fb9b6cd90d96a08
Nate Geslin <teamtomkins23@gmail.com>
2023-07-16 23:39:47 -0500

    adds the first few page routes, as simply as possible

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function AboutPage() {
+  return (
+    <div>
+      <Link href={'/'}>Home</Link>
+      About Page
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Railmaster</h1>
+      <br />
+      <Link href={'/about'}>About</Link>
+      <Link href={'/profile'}>Profile</Link>
     </main>
-  );
+  )
 }
